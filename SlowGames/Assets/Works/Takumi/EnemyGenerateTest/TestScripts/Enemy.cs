7136a8b6504c9d5323ad9b2f0fe441f049265674
佐藤 匠 <biker-duck.g@ezweb.ne.jp>
2016-10-14 18:19:21 +0900

    ResourcesフォルダにTestEnemyをつくった。 敵キャラを生成.<"G"key>で生成。 生成位置は、Generatorの場所.

---
+++ SlowGames/Assets/Works/Takumi/EnemyGenerateTest/TestScripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _deathEffect;
+    
+    //たまにあたったら死にます
+    void OnCollisionEnter(Collision other)
+    {
+       
+        if (other.gameObject.tag == "Bullet")
+        {
+            //エフェクト
+            var effect = Instantiate(_deathEffect);
+            _deathEffect.transform.position = transform.position;
+
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ SlowGames/Assets/Works/Takumi/EnemyGenerateTest/TestScripts/EnemyGenerator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum EnemyType
+{
+    Easy,
+    Normal,
+    Hard,
+
+    Last,
+}
+
+//enemyを生成
+public class EnemyGenerator : MonoBehaviour
+{
+    
+    //雑魚敵
+    [SerializeField]
+    List<GameObject> _easyEnemys = new List<GameObject>();
+    [SerializeField]
+    List<GameObject> _normalEnemys = new List<GameObject>();
+    [SerializeField]
+    List<GameObject> _hardEnemys = new List<GameObject>();
+
+    //生成するエネミーを記憶 
+    Dictionary<EnemyType,List<GameObject>> _enemysDic = new Dictionary<EnemyType, List<GameObject>>();
+
+
+    void Start()
+    {
+
+        _enemysDic.Add(EnemyType.Easy, _easyEnemys);
+        _enemysDic.Add(EnemyType.Normal, _normalEnemys);
+        _enemysDic.Add(EnemyType.Hard, _hardEnemys);
+
+    }
+
+    [SerializeField]
+    EnemyType _testGenerateType;
+
+    void Update()
+    {
+
+        //test: ”G”Keyで生成
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GenerateEnemy(_testGenerateType);    
+        }
+
+    }
+
+    //test:とりあえず敵を生成
+    public void GenerateEnemy(EnemyType enemyType)
+    {    
+
+        //選んだエネミータイプから,ランダムでpatternを選び生成.
+        var enemyList = _enemysDic[enemyType];
+        int random = Random.Range(0, (enemyList.Count));
+
+        //生成
+        GameObject enemy = enemyList[random];
+        Instantiate(enemy);
+
+        //test: ジェネレーターの場所を基準に生成
+        enemy.transform.position = transform.position;
+        enemy.transform.rotation = transform.rotation;
+
+    }
+
+}
