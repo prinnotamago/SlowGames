b1d6a88ebe0afb0e4aa98f72acf9716f93cd1d2f
ooHIROoo <hiro1215.turquoise@gmail.com>
2016-10-14 17:40:37 +0900

    プレファブ作った

--- SlowGames/Assets/Works/arise/Test/Scripts/AudioSourceTest.cs
+++ SlowGames/Assets/Works/arise/Test/Scripts/AudioSourceTest.cs
@@ -4,7 +4,18 @@ public class AudioSourceTest : MonoBehaviour
 {
     void Start()
     {
-        AudioManager.instance.play3DBgm(gameObject, 0);
+        // 3D音用BGM再生
+        AudioManager.instance.play3DBgm(gameObject, AudioName.BgmName.stage01);
+        // 2D音用BGM再生
+        AudioManager.instance.playBgm(AudioName.BgmName.stage01);
+
+        // 2D用SE停止
+        AudioManager.instance.stopSe(AudioName.SeName.Thunder);
+        // 2D用SEを全て停止
+        AudioManager.instance.stopAllSe();
+
+        // Se全ての音量変更 ※デシベル単位
+        AudioManager.instance.changeSEVolume(0.0f);
     }
 
     void Update()
