f09a9279335df7169dfc80a422ca8fdfef1b0ac9
shunta13 <ike3shunta@yahoo.co.jp>
2016-10-14 11:01:01 +0900

    ファイル削除

--- SlowGames/Assets/Scripts/SlowMotion/SlowMotion.cs
+++ SlowGames/Assets/Scripts/SlowMotion/SlowMotion.cs
@@ -12,4 +12,27 @@ public class SlowMotion : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    /// <summary>
+    /// ゲーム速度を元に戻す。
+    /// １回だけ呼ぶようにしてください。
+    /// </summary>
+    static void ResetSpeed()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    /// <summary>
+    /// ゲームの速さを変える
+    /// １回だけ呼ぶようにしてください。
+    /// </summary>
+    /// <param name="speed">
+    /// 1.0f で通常速度、
+    /// 1.0f 以上で早く、
+    /// 1.0f 以下で遅くなる
+    /// </param>
+    static void GameSpeed(float speed)
+    {
+        Time.timeScale = speed;
+    }
 }
