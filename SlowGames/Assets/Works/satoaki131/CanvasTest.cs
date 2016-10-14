5c8fcb0bf36bf75a3ca69ceda4cdca21cadf115d
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-10-14 12:10:13 +0900

    Fadeシーン仮実装(これからテスト)

---
+++ SlowGames/Assets/Works/satoaki131/CanvasTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CanvasTest : MonoBehaviour {
+
+    [SerializeField]
+    private Canvas test;
+
+    void Awake()
+    {
+        var a = Instantiate(test);
+        var localPosition = transform.localPosition;
+        localPosition.z += 3.0f;
+        a.transform.localPosition = localPosition;
+        a.worldCamera = GetComponent<Camera>();
+    }
+
+}
