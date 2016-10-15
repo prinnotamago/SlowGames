25417ec6e5d0be9aef1d5f489ae9ea688a70992c
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-10-15 12:20:07 +0900

    Reloadの実装

---
+++ SlowGames/Assets/Works/satoaki131/Reload.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerShot))]
+public class Reload : MonoBehaviour {
+
+    [SerializeField]
+    private float _reloadTime = 0.5f;
+
+    /// <summary>
+    /// trueならリロード中
+    /// falseならリロードしてない状態
+    /// </summary>
+    public bool isReload
+    {
+        get; private set;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)) //あとでリロードボタンに変更
+        {
+            StartCoroutine(ShotReload());
+        }
+    }
+
+    /// <summary>
+    /// リロード中の処理
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ShotReload()
+    {
+        var time = 0.0f;
+        isReload = true;
+        //Audioを追加する(カシャッ)
+        while(time < _reloadTime)
+        {
+            time += Time.deltaTime;
+            yield return null;
+        }
+        isReload = false;
+        //音を追加する(カチッ)
+    }
+}
