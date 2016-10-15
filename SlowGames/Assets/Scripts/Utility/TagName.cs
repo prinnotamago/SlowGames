0e4640fa002056625fd3c402481fa5c46fc7d9cd
ooHIROoo <hiro1215.turquoise@gmail.com>
2016-10-15 19:38:51 +0900

    タグネームの定数を生成するUtilityを実装

---
+++ SlowGames/Assets/Scripts/Utility/TagName.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// タグ名を定数で管理するクラス
+/// </summary>
+public static class TagName
+{
+	public const string Untagged = "Untagged";
+	public const string Respawn = "Respawn";
+	public const string Finish = "Finish";
+	public const string EditorOnly = "EditorOnly";
+	public const string MainCamera = "MainCamera";
+	public const string Player = "Player";
+	public const string GameController = "GameController";
+	public const string Enemy = "Enemy";
+	public const string Bullet = "Bullet";
+	public const string Weapon = "Weapon";
+	public const string UI = "UI";
+	public const string EnemyBullet = "EnemyBullet";
+}
