f3c975be494eb41d11202d6a8e2e5c3fbefe2ee1
ooHIROoo <hiro1215.turquoise@gmail.com>
2016-10-15 10:50:47 +0900

    3D音響用の再生が正常に再生されていなかったので修正

--- SlowGames/Assets/Works/arise/Test/Scripts/AudioSourceTest.cs
+++ SlowGames/Assets/Works/arise/Test/Scripts/AudioSourceTest.cs
@@ -4,25 +4,19 @@ public class AudioSourceTest : MonoBehaviour
 {
     void Start()
     {
-        // 3D音用BGM再生
-        AudioManager.instance.play3DBgm(gameObject, AudioName.BgmName.stage01);
-        // 2D音用BGM再生
-        AudioManager.instance.playBgm(AudioName.BgmName.stage01);
+        //// 3D音用BGM再生
+        //AudioManager.instance.play3DBgm(gameObject, AudioName.BgmName.stage01);
 
-        // 2D用SE停止
-        AudioManager.instance.stopSe(AudioName.SeName.Thunder);
-        // 2D用SEを全て停止
-        AudioManager.instance.stopAllSe();
+        AudioManager.instance.play3DSe(gameObject, AudioName.SeName.Thunder);
 
-        // Se全ての音量変更 ※デシベル単位
-        AudioManager.instance.changeSEVolume(0.0f);
+        //AudioManager.instance.playSe(AudioName.SeName.gun1);
     }
 
     void Update()
     {
-        if (Input.anyKeyDown)
-        {
-            AudioManager.instance.stop3DBgm(gameObject, AudioName.BgmName.stage01);
-        }
+        //if (Input.anyKeyDown)
+        //{
+        //    AudioManager.instance.stop3DBgm(gameObject, AudioName.BgmName.stage01);
+        //}
     }
 }
