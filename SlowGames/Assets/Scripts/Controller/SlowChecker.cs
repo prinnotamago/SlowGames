d9bbad3ddcd3b5b686eb123c54a4d25b686102af
ooHIROoo <hiro1215.turquoise@gmail.com>
2016-10-19 19:43:20 +0900

    扇形とコントローラーの当たり判定を取得できるようにした

---
+++ SlowGames/Assets/Scripts/Controller/SlowChecker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowChecker : MonoBehaviour
+{
+    public bool canSlow
+    {
+        get; private set;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        canSlow = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        canSlow = false;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        canSlow = true;
+    }
+
+    void Update()
+    {
+        Debug.Log(canSlow);
+    }
+}
---
+++ SlowGames/Assets/Works/arise/Test/Scripts/CameraColliderTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraColliderTest : MonoBehaviour
+{
+    public bool trriger = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        trriger = true;
+        Debug.Log("TrrigerEnter");
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        trriger = false;
+        Debug.Log("TrrigerExit");
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        trriger = true;
+        Debug.Log("TrrigerStay");
+    }
+
+    void Update()
+    {
+        Debug.Log(trriger);
+    }
+}
