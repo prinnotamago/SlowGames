fd9f377ddc44de398253f6eb2dc1318586d3aaec
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-10-28 15:21:03 +0900

    EnemyBulletと当たった数、生き残った時間、倒した数をとれるようにした

--- SlowGames/Assets/Works/satoaki131/Scripts/ScoreManager.cs
+++ SlowGames/Assets/Works/satoaki131/Scripts/ScoreManager.cs
@@ -9,6 +9,10 @@ public class ScoreManager : MonoBehaviour {
     private int _hitEnemyCount = 0;
     private int _shotCount = 0;
 
+    private float _lifeTimeCount = 0.0f;
+
+    private int _flipEnemyBulletCount = 0;
+
     /// <summary>
     /// インスタンスを所得
     /// </summary>
@@ -22,10 +26,21 @@ public class ScoreManager : MonoBehaviour {
     /// </summary>
     public float getHitParsent
     {
-        get{ return _hitEnemyCount / (float)_shotCount; }
+        get{ return (_hitEnemyCount / (float)_shotCount) * 100; }
     }
 
+    /// <summary>
+    /// 敵を倒した数(当たった数)
+    /// </summary>
+    public int HitEnemyCount
+    {
+        get { return _hitEnemyCount; }
+    }
 
+    public float LifeTime
+    {
+        get{ return _lifeTimeCount; }
+    }
 
     public ScoreManager AddScore()
     {
@@ -43,6 +58,16 @@ public class ScoreManager : MonoBehaviour {
     }
 
     /// <summary>
+    /// ゲーム中の時間を計測
+    /// </summary>
+    /// <returns></returns>
+    public ScoreManager GameTimeCount()
+    {
+        _lifeTimeCount += Time.unscaledTime;
+        return this;
+    }
+
+    /// <summary>
     /// 敵に当たった数を足していく
     /// </summary>
     /// <returns></returns>
@@ -50,6 +75,28 @@ public class ScoreManager : MonoBehaviour {
     {
         _hitEnemyCount++;
         return this;
+    }
+
+    /// <summary>
+    /// Enemyの弾と当たった数を足していく
+    /// </summary>
+    /// <returns></returns>
+    public ScoreManager AddFlipEnemyBulletCount()
+    {
+        _flipEnemyBulletCount++;
+        return this;
+    }
+
+    /// <summary>
+    /// 全部0に戻す
+    /// </summary>
+    public void Reset()
+    {
+        _lifeTimeCount = 0.0f;
+        _shotCount = 0;
+        _hitEnemyCount = 0;
+        _flipEnemyBulletCount = 0;
+        _score = 0;
     }
 
     void Start()
@@ -68,7 +115,6 @@ public class ScoreManager : MonoBehaviour {
 
     void Update()
     {
-        Debug.Log(getHitParsent);
     }
 
 }
