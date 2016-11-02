f01cfc624b39dfc830d0462f63b3a8ba2457d400
佐藤 匠 <biker-duck.g@ezweb.ne.jp>
2016-11-02 10:09:49 +0900

    エネミーの出現頻度, タイプ　をウェーブ管理できるようにしました 値はテスト中.

--- SlowGames/Assets/Works/Takumi/EnemyManageTest/GenerateManager.cs
+++ SlowGames/Assets/Works/Takumi/EnemyManageTest/GenerateManager.cs
@@ -2,6 +2,36 @@
 using System.Collections;
 using System.Collections.Generic;
 
+//波状攻撃のデータ構造体です
+[System.Serializable]
+public struct WaveData
+{
+    //敵の死亡数がこの値以上の場合以下の情報で敵を出現させます.
+    public int _startDieCount;
+
+    public int _generateTimingCount;     //シーン内にいるエネミーが何体死んだら再度出現させるか
+    public int _generateCount;           //何体ずつだすか
+    public int _enemyLimit;                  //同時出現数の限界値
+    public List<EnemyType> _generateTypeList;//どのタイプを出すか
+
+    public WaveData(int startDieCount,
+                    int generateTimingCount,int generateCount,
+                    int enemyLimit,List<EnemyType> generateTypeList)
+    {
+
+       _startDieCount = startDieCount;
+       
+       _generateTimingCount =  generateTimingCount;
+       _generateCount       = generateCount;
+       _generateTypeList    = new List<EnemyType>();
+       _enemyLimit          = enemyLimit;
+       _generateTypeList    =  generateTypeList;
+        
+    }
+
+}
+
+//
 public class GenerateManager : MonoBehaviour
 {
 
@@ -14,6 +44,10 @@ public class GenerateManager : MonoBehaviour
     [SerializeField,Range(1,20)]
     int _enemyLimit = 1;
 
+    [SerializeField]
+    List<WaveData> _waveDate = new List<WaveData>();
+    int _currentWaveCount = 0;
+
     //死亡数をカウントします
     int _deathCount = 0;
 
@@ -22,8 +56,12 @@ public class GenerateManager : MonoBehaviour
         //初期化
         _enemyGenerator = this.gameObject.GetComponent<EnemyGenerator>();
         _deathCount = 0;
+
         //開幕３体配置.
-        SetEnemy(3);
+        _currentWaveCount = 0;
+        var waveData = _waveDate[_currentWaveCount];
+        SetEnemy(3,waveData._generateTypeList);
+       
 
     }
 
@@ -57,11 +95,32 @@ public class GenerateManager : MonoBehaviour
     //FixMe://２体死ぬごとに、敵キャラを生成
     void UpdateEnemyCount()
     {
-        //２体死ぬごとに、敵キャラを生成
-        if (_deathCount % 2 == 0)
+
+        var waveData = _waveDate[_currentWaveCount];
+
+        //ウェーブのデータが最大にいったらそれ以上はいかない
+        Debug.Log(_currentWaveCount + "c : wabe" + _waveDate.Count);
+        if (_currentWaveCount  < _waveDate.Count - 1)
         {
-                SetEnemy(2);
+
+            //敵の死亡数が一定数行っていたらウェーブを更新
+            if (_deathCount > _waveDate[(_currentWaveCount + 1)]._startDieCount)
+            {
+                Debug.Log("更新");
+                _currentWaveCount += 1;
+
+            }
+
         }
+        //死ぬごとに、敵キャラを生成
+        if (_deathCount % waveData._generateTimingCount == 0)
+        {
+            SetEnemy(waveData._generateCount,waveData._generateTypeList);
+        }
+
+
+        //SetEnemy(_waveDate[_currentWaveCount].);
+
     }
 
     //敵を生成する場所の数、　またそこから出す敵のカウントを設定、敵キャラを配置]
@@ -69,7 +128,7 @@ public class GenerateManager : MonoBehaviour
     void SetEnemy(int count = 1,EnemyType enemyType = EnemyType.Easy)
     {
 
-        StartCoroutine(DelayGenerate(count,0.5f));
+        StartCoroutine(DelayGenerate(enemyType,count,0.5f));
 
 
 //        for (int i = 0; i < count; i++)
@@ -96,7 +155,7 @@ public class GenerateManager : MonoBehaviour
     }
 
     //一気に生成させない
-    IEnumerator DelayGenerate(int count = 1, float delayTime = 0.5f)
+    IEnumerator DelayGenerate(EnemyType enemyType, int count = 1, float delayTime = 0.5f)
     {
 
         float counter = 0;
@@ -144,5 +203,84 @@ public class GenerateManager : MonoBehaviour
 
     }
 
+    //敵を生成する場所の数、　またそこから出す敵のカウントを設定、敵キャラを配置]
+    //*敵キャラがいる場合は生成させない
+    void SetEnemy(int count,List<EnemyType> enemyTypes)
+    {
+
+        StartCoroutine(DelayGenerate(enemyTypes,count,0.5f));
+
+
+//        for (int i = 0; i < count; i++)
+//        {
+//        
+//            //地上の出現位置をランダムに取得 ,//そこに敵キャラが一定以上いたら、再取得
+//            TargetPosition generatePosition = _enemyGenerator.GetRandomGeneratePos(_currentEnemysCount, 5);
+//
+//            //位置を示さないものが帰ってきたら処理しない
+//            //なおこれはよくない処理です
+//            if (generatePosition == TargetPosition.Last)
+//            {
+//                Debug.Log("生成できませんでした。");
+//                return;
+//            }
+//
+//            //生成した場所のカウントを覚えておく
+//            _currentEnemysCount[(int)generatePosition] += 1;
+//
+//            //生成
+//            _enemyGenerator.GenerateEnemy(EnemyType.Easy, generatePosition);
+//        }
+                                
+    }
+
+    //一気に生成させない
+    IEnumerator DelayGenerate(List<EnemyType> enemyTypes, int count = 1, float delayTime = 0.5f)
+    {
+
+        float counter = 0;
+        
+        for (int i = 0; i < count; i++)
+        {
+
+            //地上の出現位置をランダムに取得 ,//そこに敵キャラが一定以上いたら、再取得
+            TargetPosition generatePosition = _enemyGenerator.GetRandomGeneratePos(_currentEnemysCount, _enemyLimit);
+
+            //位置を示さないものが帰ってきたら処理しない
+            //なおこれはよくない処理です
+            if (generatePosition == TargetPosition.Last)
+            {
+                Debug.Log("生成できませんでした。");
+
+                break;
+            }
+
+            //生成した場所のカウントを覚えておく
+            _currentEnemysCount[(int)generatePosition] += 1;
+
+            //生成
+            _enemyGenerator.GenerateEnemy(ref enemyTypes, generatePosition);
+
+            counter = delayTime;
+
+            yield return null;
+
+            while (true)
+            {
+                counter -= Time.deltaTime;
+
+                if (counter < 0)
+                {
+                  break;
+                }
+
+                yield return null;
+            }
+
+            yield return null;
+        }
+
+
+    }
   
 }
