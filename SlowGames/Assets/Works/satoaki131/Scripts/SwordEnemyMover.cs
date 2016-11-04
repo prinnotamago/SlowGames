e10a59cace9a7961bfb54da29195e0c024447066
N702-05\vantan <ike3shunta@yahoo.co.jp>
2016-11-04 13:27:43 +0900

    FreezedRotationをいじった

--- SlowGames/Assets/Works/satoaki131/Scripts/SwordEnemyMover.cs
+++ SlowGames/Assets/Works/satoaki131/Scripts/SwordEnemyMover.cs
@@ -22,7 +22,8 @@ public class SwordEnemyMover : MonoBehaviour
         _state.Add(MoveState.Approach, Approach);
         _state.Add(MoveState.Wait, Wait);
         _state.Add(MoveState.Attack, Attack);
-
+        _angle = UnityEngine.Random.Range(-50.0f, 50.0f);
+        _speed = UnityEngine.Random.Range(_zSpeedMin, _zSpeedMax);
     }
 
     void Update()
@@ -30,18 +31,22 @@ public class SwordEnemyMover : MonoBehaviour
         _state[_moveState]();
     }
 
+    private float _speed = 0.0f;
+
     private float _angle = 0.0f;
     [SerializeField]
-    private float _z = -0.05f;
+    private float _zSpeedMin = 0.05f;
+    [SerializeField]
+    private float _zSpeedMax = 0.1f;
 
     [SerializeField]
     private float _waitDistance = 4.0f;
 
     void Approach()
     {
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(new Vector3(Camera.main.transform.localPosition.x, transform.localPosition.y, Camera.main.transform.localPosition.z));
         _angle += Time.deltaTime;
-        transform.Translate(Mathf.Sin(_angle) * 0.1f, 0, _z);
+        transform.Translate(Mathf.Sin(_angle) * 0.1f, 0, _speed);
         var distance = transform.localPosition.z - Camera.main.transform.localPosition.z;
         if(distance < _waitDistance)
         {
@@ -52,11 +57,14 @@ public class SwordEnemyMover : MonoBehaviour
 
     void Wait(){}
 
+    [SerializeField]
+    private float _appatchDistance = 1.2f;
+
     void Attack()
     {
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(new Vector3(Camera.main.transform.localPosition.x, transform.localPosition.y, Camera.main.transform.localPosition.z));
         var distance = transform.localPosition.z - Camera.main.transform.localPosition.z;
-        if(distance < 1.2f) { return; }
+        if(distance < _appatchDistance) { return; }
         transform.Translate(0, 0, 0.1f);
     }
 
@@ -66,11 +74,16 @@ public class SwordEnemyMover : MonoBehaviour
     IEnumerator AttackReserve()
     {
         var time = 0.0f;
+        GetComponentInChildren<Rigidbody>().freezeRotation = true;
         while(time < _waitTime)
         {
+            GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
+            Debug.Log(GetComponentInChildren<Rigidbody>().velocity);
             time += Time.deltaTime;
             yield return null;
         }
+        GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
         _moveState = MoveState.Attack;
     }
 
