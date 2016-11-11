9441add5c2346295da0c7a3d25244ed84dcb7da2
prinnotamago <ike3shunta@yahoo.co.jp>
2016-11-11 13:53:33 +0900

    Titleシーン更新・Viveコントローラーで物をつかむスクリプト追加

---
+++ SlowGames/Assets/Scripts/Utility/ViveGrab.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveGrab : MonoBehaviour {
+
+    //[SerializeField]
+    Rigidbody _rigidbody;
+
+    SteamVR_TrackedObject[] _viveCon;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _viveCon = FindObjectsOfType<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	//	
+	//}
+
+    void OnTriggerStay(Collision col)
+    {
+        if (col.gameObject.tag == TagName.GameController)
+        {
+            bool isPick = false;
+            foreach (var viveCon in _viveCon)
+            {
+                if (viveCon.gameObject == col.gameObject)
+                {
+                    SteamVR_Controller.Device device = SteamVR_Controller.Input((int)viveCon.index);
+                    if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+                    {
+                        transform.position = col.gameObject.transform.position;
+                        _rigidbody.velocity = Vector3.zero;
+                        if (_rigidbody.useGravity)
+                        {
+                            _rigidbody.useGravity = false;
+                        }
+                        isPick = true;
+                    }
+                }
+            }
+
+            if (!isPick)
+            {
+                if (!_rigidbody.useGravity)
+                {
+                    _rigidbody.useGravity = true;
+                }
+            }
+        }
+    }
+}
