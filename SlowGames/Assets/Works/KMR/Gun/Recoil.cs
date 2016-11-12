3b9ebd421b85feef8bcc1a39246b586f28fa91d9
kazukin134 <kazukin1345@gmail.com>
2016-11-12 11:13:34 +0900

    手のモデル変えた

---
+++ SlowGames/Assets/Works/KMR/Gun/Recoil.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoil : MonoBehaviour
+{
+    Animator _animator;
+    int _animHash;
+    bool _isShot;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _animHash = Animator.StringToHash("_isShot");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void RecoilAnimation()
+    {
+        _isShot = true;
+        _animator.SetBool(_animHash, _isShot);
+        
+              
+    }
+
+    void Move()
+    {
+        _animator.Play(_animHash, 0, 0);
+    }
+
+}
