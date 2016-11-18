daba4e95d11a311c1dc225d7fe1a3d4df10bef79
2F-DT-01\vantan <ike3shunta@yahoo.co.jp>
2016-11-18 20:11:27 +0900

    タイトルで銃を落としたら元の場所に戻す

---
+++ SlowGames/Assets/Scripts/Title/GunReseter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunReseter : MonoBehaviour {
+
+    //[SerializeField]
+    Vector3 _initPos;
+    Vector3 _initRot;
+    Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        _initPos = transform.position;
+        _initRot = transform.eulerAngles;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.tag == TagName.UI)
+        {
+            //Debug.Log("とおった");
+            transform.position = _initPos;
+            transform.eulerAngles = _initRot;
+            _rigidbody.velocity = Vector3.zero;
+        }
+    }
+}
