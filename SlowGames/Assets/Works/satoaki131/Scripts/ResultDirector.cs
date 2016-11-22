b380ea6767b46b64f28ef53b362f3be6ecbb0ecb
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-11-23 08:54:40 +0900

    Rankシステム実装(使うかわからない)

--- SlowGames/Assets/Works/satoaki131/Scripts/ResultDirector.cs
+++ SlowGames/Assets/Works/satoaki131/Scripts/ResultDirector.cs
@@ -6,27 +6,11 @@ using UnityEngine.UI;
 public class ResultDirector : MonoBehaviour {
 
     [SerializeField]
-    private Text _hitPersent = null;
-
-    [SerializeField]
-    private Text _time = null;
-
-    [SerializeField]
-    private Text _inpactDamageCount = null;
-
-    [SerializeField]
     private Text _score = null;
 
     void Start()
     {
-        var resultSecondTime = (int)ScoreManager.instance.LifeTime % 60;
-        var resultMinuteTime = (int)ScoreManager.instance.LifeTime / 60;
-
-        _hitPersent.text = "ヒット率： " + ScoreManager.instance.getHitParsent + " ％";
-        _time.text = "時間： " + resultMinuteTime + "：" + resultSecondTime.ToString().PadLeft(2, '0');
-        _inpactDamageCount.text = "被弾数： " + ScoreManager.instance.getInpactDamageCount;
         _score.text = "スコア： " + ScoreManager.instance.getScore();
-
     }
 
     void Update()
