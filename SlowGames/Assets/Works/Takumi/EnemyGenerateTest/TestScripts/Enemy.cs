b24f8a8c3553e3e12bf546cc974771eb6f1f9e3a
2F-DT-01\vantan <ike3shunta@yahoo.co.jp>
2016-11-25 19:13:17 +0900

    弾の速度変更＿ほか　ナビメッシュをみえなくするとうの微調整

--- SlowGames/Assets/Works/Takumi/EnemyGenerateTest/TestScripts/Enemy.cs
+++ SlowGames/Assets/Works/Takumi/EnemyGenerateTest/TestScripts/Enemy.cs
@@ -77,7 +77,7 @@ public class Enemy : MonoBehaviour
             FindObjectOfType<GenerateManager>().AddDeathCount(_generatePostion);
             //Test:スコア
             ScoreManager.instance.AddHitEnemyCount();
-            //ScoreManager.instance.AddScore(_type);
+            ScoreManager.instance.AddScore(_type);
             Destroy(this.gameObject);
         }
     }
