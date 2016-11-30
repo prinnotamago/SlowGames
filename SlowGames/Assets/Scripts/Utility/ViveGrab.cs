38bd592f25e97ffa5ae167caf7a308090932cb30
2F-DT-01\vantan <ike3shunta@yahoo.co.jp>
2016-11-30 10:11:57 +0900

    銃を落とさないようにした。(落とすのもできるような bool を作った)

--- SlowGames/Assets/Scripts/Utility/ViveGrab.cs
+++ SlowGames/Assets/Scripts/Utility/ViveGrab.cs
@@ -19,6 +19,11 @@ public class ViveGrab : MonoBehaviour {
 
     SteamVR_TrackedObject _viveCon;
 
+    Rigidbody _pickObjRigidbody = null;
+
+    [SerializeField]
+    bool _isDrop = false;
+
     // Use this for initialization
     void Start () {
         //_rigidbody = GetComponent<Rigidbody>();
@@ -27,10 +32,17 @@ public class ViveGrab : MonoBehaviour {
 	}
 
     // Update is called once per frame
-    //void Update()
-    //{
-    //    Debug.Log(_isPick);
-    //}
+    void Update()
+    {
+        //Debug.Log(_isPick);
+        if (!_isDrop && _isPick)
+        {
+            _pickObjRigidbody.velocity = Vector3.zero;
+            _pickObj.gameObject.transform.position = transform.position;
+            _pickObj.gameObject.transform.rotation = transform.rotation;
+            _pickObj.gameObject.transform.Rotate(235, 0, 180);
+        }
+    }
 
     void OnTriggerStay(Collider col)
     {
@@ -38,25 +50,26 @@ public class ViveGrab : MonoBehaviour {
         {
             if (_pickObj == col.gameObject)
             {
-                var rigidbody = col.GetComponent<Rigidbody>();
                 SteamVR_Controller.Device device = SteamVR_Controller.Input((int)_viveCon.index);
                 if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
                 {
-                    rigidbody.velocity = Vector3.zero;
+                    _pickObjRigidbody.velocity = Vector3.zero;
                     col.gameObject.transform.position = transform.position;
                     col.gameObject.transform.rotation = transform.rotation;
                     col.gameObject.transform.Rotate(235, 0, 180);
-                    if (rigidbody.useGravity)
+                    if (_pickObjRigidbody.useGravity)
                     {
-                        rigidbody.useGravity = false;
+                        _pickObjRigidbody.useGravity = false;
                         //Debug.Log("もってる");
                     }
                 }
                 else
                 {
-                    rigidbody.useGravity = true;
+                    if (!_isDrop) { return; }
+                    _pickObjRigidbody.useGravity = true;
                     _isPick = false;
                     _pickObj = null;
+                    _pickObjRigidbody = null;
                     //Debug.Log("離した");
                 }
             }
@@ -66,6 +79,7 @@ public class ViveGrab : MonoBehaviour {
                 if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
                 {
                     _pickObj = col.gameObject;
+                    _pickObjRigidbody = col.GetComponent<Rigidbody>();
                     _isPick = true;
                 }
             }
@@ -74,12 +88,14 @@ public class ViveGrab : MonoBehaviour {
 
     void OnTriggerExit(Collider col)
     {
+        if (!_isDrop) { return; }
         if (col.gameObject.tag == _tagName)
         {
             if (_pickObj == col.gameObject)
             {
-                var rigidbody = col.GetComponent<Rigidbody>();
-                rigidbody.useGravity = true;
+                //var rigidbody = col.GetComponent<Rigidbody>();
+                _pickObjRigidbody.useGravity = true;
+                _pickObjRigidbody = null;
                 _isPick = false;
                 _pickObj = null;
             }
