2f60762eea0015e5fb6fbf862de22a552609e077
2F-DT-01\vantan <ike3shunta@yahoo.co.jp>
2016-12-02 13:39:37 +0900

    爆発エフェクト修正(後で修正)

---
+++ SlowGames/Assets/Works/KMR/Effect/ParticleStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleStart : MonoBehaviour {
+
+	void Start ()
+    {
+        foreach(var test in GetComponentsInChildren<ParticleSystem>())
+        {
+            test.Play();
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
