320823000f0c24d1c8d78c02a5b359de13914354
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-12-03 11:55:42 +0900

    ユーザー認証の演出仮追加

---
+++ SlowGames/Assets/Works/satoaki131/Scripts/UserAuthenticationText.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserAuthenticationText : MonoBehaviour {
+
+    public enum TextType
+    {
+        point,
+        Descript,
+        Clear
+    }
+
+    [SerializeField]
+    private TextType _type = TextType.Descript;
+
+    private Text _text = null;
+
+    private float _time = 0.0f;
+    private float _testTime = 0.5f;
+
+    void Start()
+    {
+        GetComponent<Text>();
+        if(_type == TextType.Descript)
+        {
+            _text.text = "ユーザー認証中";
+        }
+        else if(_type == TextType.Clear)
+        {
+            _text.text = "確認しました。";
+        }
+        else if(_type == TextType.point)
+        {
+            _text.text = "";
+        }
+    }
+
+    void Update()
+    {
+        if (_type != TextType.point) return;
+        _time += Time.deltaTime;
+        if(_time > _testTime)
+        {
+            _testTime += 0.5f;
+            _text.text += ".";
+        }
+        if(_testTime > 2.0f)
+        {
+            _testTime = 1.0f;
+            _text.text = "";
+
+        }
+    }
+
+
+}
