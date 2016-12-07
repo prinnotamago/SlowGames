8521f386c13015a8b53d6931eda117f617185df8
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-12-07 15:31:55 +0900

    ResultシーンからTitleに戻れようにした

---
+++ SlowGames/Assets/Works/satoaki131/Scripts/ResultManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private PutGunStand[] _put = null;
+
+    void Start()
+    {
+        _put = GetComponentsInChildren<PutGunStand>();
+    }
+
+    void Update()
+    {
+        //デバッグ用:台座に置いてないときでも戻れるように
+        if(Input.GetKeyDown(KeyCode.Backspace))
+        {
+            SceneChange.ChangeScene(SceneName.Name.Title, Color.white);
+        }
+
+        if (!_put[0].isPutGun) return;
+        if (!_put[1].isPutGun) return;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneChange.ChangeScene(SceneName.Name.Title, Color.white);
+        }
+    }
+
+}
