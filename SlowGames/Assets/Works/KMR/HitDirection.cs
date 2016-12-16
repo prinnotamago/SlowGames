2ef1c77fb26808e4c121b92aaac9d2054ca82bff
kazukin134 <kazukin1345@gmail.com>
2016-12-16 11:58:06 +0900

    ヒットエフェクト変更

---
+++ SlowGames/Assets/Works/KMR/HitDirection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDirection : MonoBehaviour
+{
+
+    GameObject a;
+
+    PlayerHP _playerHP;
+
+    void Start()
+    {
+        a = GameObject.FindGameObjectWithTag(TagName.Player);
+        _playerHP = a.GetComponent<PlayerHP>();
+        DirectionMove(a.transform.position,_playerHP.EnemyPos);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void DirectionMove(Vector3 player,Vector3 enemy)
+    {
+        var enemyDirection = (enemy - player).normalized;
+        float angle = Vector3.Angle(player, enemyDirection);
+        gameObject.transform.Rotate(0,angle,0);
+    }
+
+}
