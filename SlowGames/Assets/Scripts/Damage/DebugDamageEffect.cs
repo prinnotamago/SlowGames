24329a3df24c1a08494d6f7abe7c8b6c44a51a58
2F-DT-01\vantan <ike3shunta@yahoo.co.jp>
2016-12-16 19:58:12 +0900

    アルファ前にすべての変更を更新

---
+++ SlowGames/Assets/Scripts/Damage/DebugDamageEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugDamageEffect : MonoBehaviour {
+
+    float damage = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            damage += 0.1f;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SteamVR_Fade.Start(new Color(damage, 0.5f, 0.5f, 0.5f), 0.0f, true);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SteamVR_Fade.Start(Color.clear, 1.0f, true);
+            damage = 0;
+        }
+    }
+}
