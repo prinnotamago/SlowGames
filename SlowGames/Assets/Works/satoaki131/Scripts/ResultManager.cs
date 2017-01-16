c883a1a145b2cd7409e668de20bec29116bddbf3
Satoaki131 <akinori_0131@yahoo.co.jp>
2017-01-16 15:58:54 +0900

    Resultを一つのPrefabにした、ResultManagerの中身を更新

--- SlowGames/Assets/Works/satoaki131/Scripts/ResultManager.cs
+++ SlowGames/Assets/Works/satoaki131/Scripts/ResultManager.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class ResultManager : MonoBehaviour
 {
 
+    public enum State
+    {
+        GunPut,
+        End,
+        Wait
+    }
+    private Dictionary<State, Action> _stateUpdate = null;
+    private State _state = State.GunPut;
+
     [SerializeField]
     private PutGunStand[] _put = null;
 
-    //void Start()
-    //{
-    //    _put = GetComponentsInChildren<PutGunStand>();
-    //}
+    [SerializeField]
+    private Canvas _logoCanvas = null;
+
+    void Start()
+    {
+        _stateUpdate = new Dictionary<State, Action>();
+        _stateUpdate.Add(State.GunPut, GunPutUpdate);
+        _stateUpdate.Add(State.Wait, () => { });
+        _stateUpdate.Add(State.End, EndUpdate);
+    }
 
     void Update()
     {
@@ -21,15 +37,32 @@ public class ResultManager : MonoBehaviour
             SceneChange.ChangeScene(SceneName.Name.Title, Color.white);
         }
 
+        _stateUpdate[_state]();
+    }
+
+    void GunPutUpdate()
+    {
         if (!_put[0].isPutGun) return;
         if (!_put[1].isPutGun) return;
-        
-        //演出入れるならここに追加する
 
+        _state = State.Wait;
+        StartCoroutine(Production());
+    }
+
+    private IEnumerator Production()
+    {
+        //thank you for playingとlogo演出
+        yield return null;
+        _state = State.End;
+    }
+
+    void EndUpdate()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneChange.ChangeScene(SceneName.Name.Title, Color.white);
         }
     }
+
 
 }
