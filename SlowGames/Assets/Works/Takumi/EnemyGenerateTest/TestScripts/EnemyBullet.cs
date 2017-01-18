af409a037c381250ac77ccd112ad0afbb5504225
佐藤 匠 <biker-duck.g@ezweb.ne.jp>
2017-01-18 18:16:34 +0900

    玉がはねたときの音追加.

--- SlowGames/Assets/Works/Takumi/EnemyGenerateTest/TestScripts/EnemyBullet.cs
+++ SlowGames/Assets/Works/Takumi/EnemyGenerateTest/TestScripts/EnemyBullet.cs
@@ -93,7 +93,7 @@ public class EnemyBullet : MonoBehaviour {
                 effect.transform.position = transform.position;
                 //音
                 //
-                //AudioManager.instance.play3DSe(effect, AudioName.SeName.Thunder);
+                AudioManager.instance.play3DSe(effect, AudioName.SeName.BulletFlip);
 
                 //判定消す
                 _isBlow = true;
