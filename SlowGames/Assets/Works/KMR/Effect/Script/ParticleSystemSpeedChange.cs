7d0079ad78bfab506c30dd245becb11a83d0628e
kazukin134 <kazukin1345@gmail.com>
2017-01-20 21:04:51 +0900

    エフェクトが消えないバグ修正

---
+++ SlowGames/Assets/Works/KMR/Effect/Script/ParticleSystemSpeedChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemSpeedChange : MonoBehaviour
+{
+
+    ParticleSystem _particleSystem;
+
+    void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+
+    void Update()
+    {
+        if (!SlowMotion._instance.isSlow)
+        {
+            _particleSystem.playbackSpeed = 1;
+            return;
+        }
+
+
+        if (SlowMotion._instance.isSlow)
+        {
+            _particleSystem.playbackSpeed = 1  * SlowMotion._instance.RealSpeed();
+        }
+
+        //Debug.Log(_particleSystem.playbackSpeed);
+
+    }
+}
---
+++ SlowGames/Assets/Works/KMR/Effect/Script/TestA.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestA : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject a;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            var b = Instantiate(a);
+        }
+    }
+}
