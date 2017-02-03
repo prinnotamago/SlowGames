ed891f2f492f168cecff63c65a1c10d75ad50f0f
kazukin134 <kazukin1345@gmail.com>
2017-02-03 10:40:05 +0900

    スローモーション中に出るエフェクトのスクリプト追加

---
+++ SlowGames/Assets/Works/KMR/Effect/OverHeatEffectCreate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverHeatEffectCreate : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject overHeatEffect = null;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!SlowMotion._instance.limiterFlag)
+        {
+            if (overHeatEffect.activeInHierarchy == true)
+            {
+                overHeatEffect.SetActive(false);
+            }
+            return;
+        }
+
+
+        if (overHeatEffect.activeInHierarchy == false)
+        {
+            overHeatEffect.SetActive(true);
+        }
+    }
+}
