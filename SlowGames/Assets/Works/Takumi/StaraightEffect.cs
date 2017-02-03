9b6e126cd106d7ba92219cf5c4ed1009ff081d44
佐藤 匠 <biker-duck.g@ezweb.ne.jp>
2017-02-03 19:44:46 +0900

    銃エフェクトテスト

---
+++ SlowGames/Assets/Works/Takumi/StaraightEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaraightEffect : MonoBehaviour {
+
+	[SerializeField]
+    float _moveSpeed = 100.0f;
+    [SerializeField]
+    float _deathTime = 3;
+
+    void Start()
+    {
+        Destroy(gameObject,_deathTime);
+        gameObject.transform.parent = null;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		    transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * _moveSpeed;
+	}
+}
