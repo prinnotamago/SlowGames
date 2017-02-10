d566b8cb44969cef15f56022719795d4581cd609
2F-DT-01\vantan <ike3shunta@yahoo.co.jp>
2017-02-10 15:35:01 +0900

    追加

---
+++ SlowGames/Assets/Works/KMR/ShotCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter(Collision col) //子供のあたり判定のときも呼んでくれる
+    {
+
+        if (col.gameObject.tag == "Weapon" || col.gameObject.tag == "Bullet" || col.gameObject.tag == "Player" || col.gameObject.tag == "Floor") return;
+        Destroy(gameObject);
+    }
+
+}
